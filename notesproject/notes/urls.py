61223794415850657299696712fbbc43d530ce31
Isaiah Ellis <isaiahellis1234@gmail.com>
2025-03-17 13:09:41 -0700

    Delete notesproject directory

--- notesproject/notes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import frontpage, create_note
-from . import views
-
-urlpatterns = [
-    path('', views.frontpage, name='frontpage'),
-    path('create/', views.create_note, name='create-note')
-]
